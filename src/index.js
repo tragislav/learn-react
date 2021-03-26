6b2db4e7072c252a2a0dba15d9de9d03182fa032
Tragislav Work PC <s.dykomenko@gmail.com>
2021-03-26 16:28:38 +0300

    state and props lesson

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app';
+// import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 26,
+        };
+        // this.nextYear = this.nextYear.bind(this);
+        this.nextYear = () => {
+            this.setState((state) => ({
+                years: ++state.years,
+            }));
+        };
+    }
+    // nextYear() {
+    //         this.setState((state) => ({
+    //             years: ++state.years,
+    //         }));
+    // }
+    render() {
+        const { name, surname, link } = this.props;
+        const { years } = this.state;
+        return (
+            <>
+                <button onClick={this.nextYear}>++</button>
+                <h1>
+                    My name is {name}, surname - {surname}, years - {years}
+                </h1>
+                <a href={link}>My profile</a>
+            </>
+        );
+    }
+}
+
+const All = () => {
+    return (
+        <>
+            <WhoAmI name="John" surname="Smith" link="facebook.com" />
+            <WhoAmI name="Ivan" surname="Smit" link="vk.com" />
+            <WhoAmI name="Alex" surname="Shepard" link="facebook.com" />
+        </>
+    );
+};
+
+ReactDOM.render(<All />, document.getElementById('root'));
