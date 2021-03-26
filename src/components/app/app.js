9ad35f321c2a6af66581b5a86eb03ddb041df17a
Tragislav Work PC <s.dykomenko@gmail.com>
2021-03-26 17:45:18 +0300

    change post-list/-list-item, modified code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+    const data = [
+        { label: 'Going to learn React', important: true, id: 'qwe' },
+        { label: 'Going to learn React', important: true, id: 'sdffd' },
+        { label: 'That is so good and tasty', important: false, id: 'jdfjh' },
+        { label: 'I need you feedback...', important: false, id: 'ewfdf' },
+    ];
+
     return (
         <div className="app">
             <AppHeader />
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel />
                 <PostStatusFilter />
             </div>
-            <PostList />
+            <PostList posts={data} />
             <PostAddForm />
         </div>
     );
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,14 +3,17 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
-    return (
-        <ul className="app-list list-group">
-            <PostListItem />
-            <PostListItem />
-            <PostListItem />
-        </ul>
-    );
+const PostList = ({ posts }) => {
+    const elements = posts.map((item) => {
+        const { id, ...itemProps } = item; // Деструкторизация обьекта
+        return (
+            <li key={id} className="list-group-item">
+                <PostListItem {...itemProps} />
+            </li>
+        );
+    });
+
+    return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default PostList;
