7ccf74fd823f0040c51194b0097bc04db9787aa9
Tragislav Work PC <s.dykomenko@gmail.com>
2021-03-29 09:40:28 +0300

    Функционал добавления поста, удаления, поиска и фильтрации

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './app-header.css';
 
-const AppHeader = () => {
+const AppHeader = ({ liked, allPosts }) => {
     return (
         <div className="app-header d-flex">
             <h1>Tomas Red</h1>
-            <h2>5 записей, из них понравилось 0</h2>
+            <h2>
+                {allPosts} записей, из них понравилось {liked}
+            </h2>
         </div>
     );
 };
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import PostListItem from '../post-list-item';
 
+import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = ({ posts }) => {
+const PostList = ({ posts, onDelete, onToggleImportant, onToggleLiked }) => {
     const elements = posts.map((item) => {
-        const { id, ...itemProps } = item; // Деструкторизация обьекта
+        const { id, ...itemProps } = item;
         return (
             <li key={id} className="list-group-item">
-                <PostListItem {...itemProps} />
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}
+                />
             </li>
         );
     });
